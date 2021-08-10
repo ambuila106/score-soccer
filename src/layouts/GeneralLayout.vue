40b73a67a38cedba3dbbfb43eaefe09107b7e965
ambuila106 <brandon.ambuila@unillanos.edu.co>
2021-08-10 01:38:43 -0500

    added general layout, scss, sass

---
+++ src/layouts/GeneralLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <select>
+    <option>La primera opción</option>
+    <option>La segunda opción</option>
+    <option>La tercera opción</option>
+  </select>
+
+  <select>
+    <option>La primera opción</option>
+    <option>La segunda opción</option>
+    <option>La tercera opción</option>
+  </select>
+
+  <router-view />
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,18 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import GeneralLayout from "../layouts/GeneralLayout.vue";
 import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "",
+    component: GeneralLayout,
+    children: [
+      {
+        path: "",
+        name: "Home",
+        component: Home,
+      },
+    ]
   },
 ];
 
