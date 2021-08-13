55760b2c39fdfac94ecafe237d20e638ec319671
ambuila106 <brandon.ambuila@unillanos.edu.co>
2021-08-12 23:39:06 -0500

    utitlities and import moment js

--- src/components/MatchCard.vue
+++ src/components/MatchCard.vue
@@ -7,14 +7,22 @@
     <div class="match-card__content">
       <h4>{{ match.title }}</h4>
       <p>{{ match.competition.name }}</p>
-      <span class="text-small">{{ match.date }}</span>
+      <span class="text-small">{{ dateToLanguage(match.date) }}</span>
     </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
   props: ["match"],
   name: "match-card",
+
+  setup() {
+    function dateToLanguage(date) {
+      return moment(String(date)).format("LLLL");
+    }
+    return { dateToLanguage }
+  }
 }
 </script>
